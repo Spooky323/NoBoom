91d3c2441feb826148fb4871c0f053a9559f5119
Spooky323 <niveytan2002@gmail.com>
2020-12-05 22:46:39 +0200

    Add project files.

---
+++ NoBoom/BombEffectPatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+namespace NoBoom
+{
+    [HarmonyPatch(typeof(BombExplosionEffect))]
+    [HarmonyPatch("SpawnExplosion")]
+    class BombEffectPatch
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
---
+++ NoBoom/Plugin.cs
@@ -0,0 +1,73 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+using HarmonyLib;
+using System.Reflection;
+
+namespace NoBoom
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        public const string HarmonyId = "com.github.Spooky323.NoBoom";
+        internal static readonly Harmony harmony = new Harmony("com.github.Spooky323.NoBoom");
+
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            Log.Info("NoBoom initialized.");
+        }
+
+        #region BSIPA Config
+        //Uncomment to use BSIPA's config
+        /*
+        [Init]
+        public void InitWithConfig(Config conf)
+        {
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Log.Debug("Config loaded");
+        }
+        */
+        #endregion
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Log.Debug("OnApplicationStart");
+            try
+            {
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+                Log.Notice("NoBoom Patched to the game");
+            }
+            catch (Exception e)
+            {
+                Log.Info("Error patching NoBoom");
+                Log.Warn(e);
+            }
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            Log.Debug("OnApplicationQuit");
+
+        }
+    }
+}
